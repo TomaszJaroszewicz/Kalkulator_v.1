e836a42b6cc27e149e4afcb0930532b9a2438d54
Tomasz Jaroszewicz <jaroszewiczt@gmail.com>
2018-02-04 23:09:55 +0100

    Naprawiony

--- WindowsFormsApp1/Properties/Resources.Designer.cs
+++ WindowsFormsApp1/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace WindowsFormsApp1.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap DSC_1656 {
-            get {
-                object obj = ResourceManager.GetObject("DSC_1656", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
